9dac05d1624d8db7c78272ca732b79db9b440ac8
Jafar Kuku <gbolade.kuku@gmail.com>
2016-12-13 17:56:26 +0000

    change
    change

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,17 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- $("#main").append("Jafar Kuku");
+ var awesomethoughts
+awesomethoughts="I am Jafar and i am awesome";
+
+var funthoughts= awesomethoughts.replace("awesome", "fun");
+$("#main").append(funthoughts);
+
+var formattedName;
+var formattedRole;
+
+formattedName = HTMLheaderName.replace("%data%", "Jafar Kuku");
+formattedRole = HTMLheaderRole.replace("%data%", "Digital Designer");
+
+$("#header").prepend(formattedName,formattedRole); 
+

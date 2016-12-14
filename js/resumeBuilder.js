8c26e672b6fad005147f29cf3e130fa70e05319c
Jafar Kuku <gbolade.kuku@gmail.com>
2016-12-14 17:18:22 +0000

    changes
    changes

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,7 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- var awesomethoughts
+var awesomethoughts
 awesomethoughts="I am Jafar and i am awesome";
 
 var funthoughts= awesomethoughts.replace("awesome", "fun");
@@ -14,4 +14,35 @@ formattedName = HTMLheaderName.replace("%data%", "Jafar Kuku");
 formattedRole = HTMLheaderRole.replace("%data%", "Digital Designer");
 
 $("#header").prepend(formattedName,formattedRole); 
+
+var skills =["Illustration, ","Animation, ", "Web Development"]; 
+
+var bio = {
+	"name" : "Jafar Kuku",
+	"role" : "Digital Media Designer",
+	"contact info":"gbolade.kuku@gmail.com",
+	"picture URL":"images/fry.jpg",
+	"welcome message":"Hi and welcome to my online resume",
+	"skills":skills
+};
+
+$("#header").append(bio.skills);
+
+var workinfo={};
+workinfo.position = " Senior Digital Designer";
+workinfo.employer = "Fuse Universal";
+workinfo.yearsworked = "5 years";
+workinfo.workcity = "London";
+
+$("#header").append(workinfo.position);
+
+var education={};
+education["lastschool"] = " University of Portsmouth";
+education["schoolyears"] = 2;
+education["schoolcity"] = "Portsmouth";
+
+$("#header").append(education["lastschool"]);
+
+
+
 
